8ddf190f10e781c033a90a9e7fd37ea89c5898f0
kkinter <ifol1129@gmail.com>
2024-03-30 01:34:50 +0900

    update for db

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
@@ -15,29 +13,6 @@ app = FastAPI()
 
 def get_db():
     db = SessionLocal()
-
-    if db.query(Song).count() == 0:
-        songs = [
-            Song(
-                title="Smells Like Teen Spirit",
-                artist="Nirvana",
-                release_date=date(1991, 9, 10),
-            ),
-            Song(
-                title="Here Comes The Sun",
-                artist="The Beatles",
-                release_date=date(1969, 8, 19),
-            ),
-            Song(
-                title="Karma Police", artist="Radiohead", release_date=date(1997, 8, 25)
-            ),
-            Song(title="Get Lucky", artist="Daft Punk", release_date=date(2013, 4, 19)),
-        ]
-
-    for song in songs:
-        db.add(song)
-
-    db.commit()
     try:
         yield db
     finally:
